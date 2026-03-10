d6aa4c02074afeb29d34c2182d7a058f85efb7cc
Diego <Scrilach1.diego.alfonso@universo.univates.br>
2026-03-09 21:44:26 -0300

    Excluido controller anterior pois era teste, Fiz um configurando rotas, e a resposta que eu queria no GET, deu certo. Proxima etapa é fazer os outros endpoints POST, PUT e DELETE

---
+++ CSharp/Projetos/ApiBoletos/ApiBoletos/Boleto.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiBoletos.Models;
+
+public class Boleto
+{
+    public int Id { get; set; }
+
+    public string Sacado { get; set; }
+
+    public decimal Valor { get; set; }
+
+    public DateTime DataVencimento { get; set; }
+
+    public bool Pago { get; set; }
+
+    public DateTime? DataPagamento { get; set; }
+}
