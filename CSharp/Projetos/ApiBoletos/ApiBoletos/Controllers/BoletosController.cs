9f28aa0dc4558db6409e5604c5dd5201399df50d
Diego <Scrilach1.diego.alfonso@universo.univates.br>
2026-03-10 23:25:02 -0300

    Criando BoletosController e removendo controller de teste

---
+++ CSharp/Projetos/ApiBoletos/ApiBoletos/Controllers/BoletosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiBoletos.Controllers;
+
+namespace ApiBoletos.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BoletosController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Lista de Boletos");
+    }
+}
